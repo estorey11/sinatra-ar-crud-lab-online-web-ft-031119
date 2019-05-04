1ab895fa7efc835a3434c24aa9b9c4ed1ca22447
Eric Storey <estorey@gmail.com>
2019-05-04 22:16:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,26 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  post '/articles' do
+    @article=Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
+  
+  get '/articles' do
+    @articles=Article.all
+    erb :index
+  end
+  
+  get '/articles/:id' do
+    @article=Article.find(params[:id])
+    erb :show
+  end
+  
+  get '/' do
+  end
 end
